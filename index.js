494dec68c5a0c2380cccad9006b9a88fdf5a5d10
VihangaTheTurtle <vihanga1432@gmail.com>
2022-10-09 06:14:46 +0100

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
 const fs = require("fs");
 
 const port = 3000;
@@ -7,11 +8,21 @@ const hostName = "pi.wifi";
 const bypassHostCheck = true;
 const app = express();
 
-const users = {
+const tokens = {};
+
+var users = {
     "0": {
         username: "Vinga",
-        passwordHash: ""
+        level: "admin",
+        authHash: "2f90863de9bce92fe031f503fb49a19b2c9a5bf9da2b5176bf15333560b5103d03198a35494f9899ecff1dd788a4ca5e32b282396ba4fca3bb32e686e0dc7707"
     }
+}
+
+function getUserFromToken(token) {
+    for (let i = 0; i < Object.keys(users).length; i++) {
+        if (Object.keys(users[Object.keys(users)[i]]).includes("token")) return users[Object.keys(users)[i]];
+    }
+    return undefined;
 }
 
 function getPage(filename) {
@@ -21,6 +32,17 @@ function getPage(filename) {
         console.error("Failed to load file:", filename, "error:", e);
         return "Failed to load page, please try again later";
     }
+}
+
+function SHA512(text) {
+    const hasher = crypto.createHash("sha512");
+    hasher.update(text)
+    return hasher.digest("hex");
+}
+
+function generateNewToken() {
+    const seed = Math.round(Math.random() * 99999999).toString() + Math.random().toString();
+    return SHA512(seed);
 }
 
 app.use(bodyParser.json());
@@ -41,18 +63,70 @@ app.get("/auth", (req, res) => {
 
 app.post("/auth", (req, res) => {
     var success = false;
+    
+    const hash = SHA512("PIIR" + req.body.password + "Auth-details");
+    var token = undefined;
+    var userId = undefined;
 
     for(let i = 0; i < Object.keys(users).length; i++) {
         const user = users[Object.keys(users)[i]];
-        console.log(req.body)
-        if (user.username === req.body.username) {
+        if (user.username === req.body.username && user.authHash === hash) {
+            console.log("Successfully verified user", user.username);
             success = true;
+            userId = Object.keys(users)[i];
+            break
         }
+    }
+
+    if (success) token = generateNewToken()
+
+    if (token !== undefined) {
+        users[userId].token = token
     }
 
     res.status(success ? 200 : 400).json({
         error: !success,
-        token: success ? "token" : undefined,
+        message: !success ? "Invalid username or password" : undefined,
+        userId: userId,
+        token: success ? token : undefined,
+        account: !success ? undefined : {
+            username: users[userId].username,
+            level: users[userId].level
+        }
+    });
+});
+
+app.get("/me", (req, res) => {
+
+    const cookiesList = req.get("cookie").split("; ");
+    var cookies = {};
+
+    for (let i = 0; i < cookiesList.length; i++) {
+        const c = cookiesList[i];
+
+        if (c === "") continue
+        if (!c.includes("=")) continue
+
+        const objects = c.split("=");
+
+        if (objects.length < 2) continue
+
+        cookies[objects[0]] = objects[1];
+    }
+
+    if (Object.keys(cookies).includes("auth") && cookies.auth.length === 128 && getUserFromToken(cookies.auth) !== undefined) {
+        var tdata = JSON.parse(JSON.stringify(getUserFromToken(cookies.auth)));
+        tdata.authHash = undefined;
+        tdata.token = undefined;
+        return res.status(200).json({
+            error: false,
+            account: tdata
+        });
+    }
+
+    res.status(401).json({
+        error: true,
+        message: "You are not authorised to use this endpoint"
     })
 });
 
